dc2e72efcc55c02230848335a22deb2d229e212d
Kenichi Ohwada <ml.ohwada@gmail.com>
2009-03-19 15:31:31 +0000

    v0.10

---
+++ xoops_trust_path/modules/timeline/class/inc/oninstall.php
@@ -0,0 +1,38 @@
+<?php
+// $Id: oninstall.php,v 1.1 2009/03/19 15:31:31 ohwada Exp $
+
+//=========================================================
+// timeline module
+// 2009-03-15 K.OHWADA
+//=========================================================
+
+if( ! defined( 'XOOPS_TRUST_PATH' ) ) die( 'not permit' ) ;
+
+//=========================================================
+// class timeline_inc_oninstall
+//=========================================================
+class timeline_inc_oninstall extends timeline_base_inc_oninstall
+{
+
+//---------------------------------------------------------
+// constructor
+//---------------------------------------------------------
+function timeline_inc_oninstall()
+{
+	$this->timeline_base_inc_oninstall();
+	$this->set_trust_dirname( TIMELINE_TRUST_DIRNAME );
+}
+
+function &getInstance()
+{
+	static $instance;
+	if (!isset($instance)) {
+		$instance = new timeline_inc_oninstall();
+	}
+	return $instance;
+}
+
+// --- class end ---
+}
+
+?>
--- xoops_trust_path/modules/timeline/class/inc/xoops_version.php
+++ xoops_trust_path/modules/timeline/class/inc/xoops_version.php
@@ -1,5 +1,5 @@
 <?php
-// $Id: xoops_version.php,v 1.1 2009/03/19 14:41:43 ohwada Exp $
+// $Id: xoops_version.php,v 1.2 2009/03/19 15:31:31 ohwada Exp $
 
 //=========================================================
 // timeline module
@@ -13,9 +13,9 @@ if( ! defined( 'XOOPS_TRUST_PATH' ) ) die( 'not permit' ) ;
 //=========================================================
 class timeline_inc_xoops_version extends timeline_base_inc_xoops_version
 {
-	var $_HAS_MAIN   = true ;
-	var $_HAS_ADMIN  = true ;
-	var $_HAS_CONFIG = false ;
+	var $_HAS_ONINSATLL = true ;
+	var $_HAS_MAIN      = true ;
+	var $_HAS_ADMIN     = true ;
 
 //---------------------------------------------------------
 // constructor
@@ -46,14 +46,6 @@ function modversion()
 function get_version()
 {
 	return _C_TIMELINE_VERSION ;
-}
-
-//---------------------------------------------------------
-// Config Settings
-//---------------------------------------------------------
-function build_config()
-{
-	return array();
 }
 
 // --- class end ---
