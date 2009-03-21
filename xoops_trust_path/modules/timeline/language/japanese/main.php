a6fd05fb7236a056b051b446086747348cf17cb9
Kenichi Ohwada <ml.ohwada@gmail.com>
2009-03-21 15:53:54 +0000

    v0.10

--- xoops_trust_path/modules/timeline/language/japanese/main.php
+++ xoops_trust_path/modules/timeline/language/japanese/main.php
@@ -1,5 +1,5 @@
 <?php
-// $Id: main.php,v 1.1 2009/03/19 14:41:41 ohwada Exp $
+// $Id: main.php,v 1.2 2009/03/21 15:53:54 ohwada Exp $
 
 //=========================================================
 // timeline module
@@ -19,6 +19,7 @@ define("_TIMELINE_TITLE_SIMPLE_EVENTS",  "Simple Eventes");
 define("_TIMELINE_TITLE_PAINTER_JSON",   "Painter JSON");
 define("_TIMELINE_TITLE_PAINTER_EVENTS", "Painter Eventes");
 define("_TIMELINE_JS_INVALID", "貴方のブラウザでは JavaScript が使用できません");
+define("_TIMELINE_CAUTION_IE", "InternetExpolar では表示できないことがあります。<br />Firfox, Opera, Safari など他のブラウザにてお試しください。");
 
 // === define end ===
 }
