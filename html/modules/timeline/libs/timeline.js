26366f34ea565b92ea69f2ce80e3882f298c6b8f
Kenichi Ohwada <ml.ohwada@gmail.com>
2011-12-26 05:47:21 +0000

    v0.20

--- html/modules/timeline/libs/timeline.js
+++ html/modules/timeline/libs/timeline.js
@@ -1,7 +1,14 @@
 /* ========================================================
- * $Id: timeline.js,v 1.2 2009/03/23 00:52:45 ohwada Exp $
+ * $Id: timeline.js,v 1.3 2011/12/26 05:47:21 ohwada Exp $
  * http://code.google.com/p/simile-widgets/wiki/Timeline
  * ========================================================
+ */
+
+/* --------------------------------------------------------
+ * change log
+ * 2011-12-25
+ * timeline_monet_xml()
+ * --------------------------------------------------------
  */
 
 var timeline_obj;
@@ -14,7 +21,7 @@ function timeline_simple_xml( param )
 
 	timeline_obj = Timeline.create(document.getElementById(param["element"]), bandinfos);
 
-	Timeline.loadXML( param["xml"], function(xml, url) { 
+	timeline_obj.loadXML( param["xml"], function(xml, url) { 
 		eventSource.loadXML(xml, url); });
 }
 
@@ -39,7 +46,7 @@ function timeline_painter_json( param )
 
 	timeline_obj = Timeline.create( document.getElementById( param["element"] ), bandinfos, Timeline.HORIZONTAL );
 
-	Timeline.loadJSON( param["json"], function(json, url) { 
+	timeline_obj.loadJSON( param["json"], function(json, url) { 
 		eventSource.loadJSON(json, url); });
 }
 
@@ -65,6 +72,30 @@ function timeline_painter_events( param, event_array )
 	timeline_obj.getBand(0).setCenterVisibleDate( center_date );
 }
 
+function timeline_monet_xml( param ) 
+{
+	var eventSource = new Timeline.DefaultEventSource(0);
+	var bandInfos   = timeline_init_bandinfos_monet( eventSource, param );
+
+	timeline_obj = Timeline.create( document.getElementById(param["element"]), bandInfos, Timeline.HORIZONTAL );
+
+	timeline_obj.loadXML( param["xml"], function(xml, url) { 
+		eventSource.loadXML(xml, url); });
+}
+
+function timeline_monet_events( param, event_array ) 
+{
+	var eventSource = new Timeline.DefaultEventSource(0);
+	var bandInfos   = timeline_init_bandinfos_monet( eventSource, param );
+
+	for( i=0 ; i< event_array.length ; i++ ){
+		var event = new Timeline.DefaultEventSource.Event( event_array[i] );
+		eventSource.add( event );
+	}
+
+	timeline_obj = Timeline.create( document.getElementById(param["element"]), bandInfos, Timeline.HORIZONTAL );
+}
+
 function timeline_init_bandinfos( eventSource, param ) 
 {
 	var bandinfos = [
@@ -87,6 +118,7 @@ function timeline_init_bandinfos( eventSource, param )
 
 	bandinfos[1].syncWith  = param["band_1_syncwith"];
 	bandinfos[1].highlight = param["band_1_highlight"];
+
 	return bandinfos;
 }
 
@@ -140,6 +172,57 @@ function timeline_init_bandinfos_painter( eventSource, param )
 	bandinfos[1].syncWith = 0;
 	bandinfos[1].highlight = true;
 	return bandinfos;
+}
+
+function timeline_init_bandinfos_monet( eventSource, param ) 
+{
+	var theme = Timeline.ClassicTheme.create();
+	theme.event.bubble.width = 320;
+	theme.event.bubble.height = 220;
+	theme.ether.backgroundColors[1] = theme.ether.backgroundColors[0];
+	var d = Timeline.DateTime.parseGregorianDateTime( param["band_year"] )
+
+	var bandInfos = [
+		Timeline.createBandInfo({
+			width:          "10%", 
+			intervalUnit:   Timeline.DateTime.DECADE, 
+			intervalPixels: param["band_0_pixels"],
+			date:           d,
+			showEventText:  false,
+			theme:          theme
+		}),
+		Timeline.createBandInfo({
+			width:          "90%", 
+			intervalUnit:   Timeline.DateTime.DECADE, 
+			intervalPixels: param["band_1_pixels"],
+			eventSource:    eventSource,
+			date:           d,
+			theme:          theme
+		})
+	];
+
+	bandInfos[0].etherPainter = new Timeline.YearCountEtherPainter({
+		startDate:  "Nov 14 1840 00:00:00 GMT",
+		multiple:   5,
+		theme:      theme
+	});
+
+	bandInfos[0].syncWith = 1;
+	bandInfos[0].highlight = false;
+
+	bandInfos[0].decorators = [
+		new Timeline.SpanHighlightDecorator({
+			startDate:  "Nov 14 1840 00:00:00 GMT",
+			endDate:    "Dec 05 1926 00:00:00 GMT",
+			startLabel: "birth",
+			endLabel:   "death",
+			color:      "#FFC080",
+			opacity:    50,
+			theme:      theme
+		})
+	];
+
+	return bandInfos;
 }
 
 function timeline_get_center_date()
